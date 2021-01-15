4186840179a2791e7bc0461c0c1c72e10d93aa96
balib01 <balib1437@gmail.com>
2021-01-15 15:06:42 +0400

    modify

--- SCA/sca_week2/script.js
+++ SCA/sca_week2/script.js
@@ -1,34 +1,26 @@
 //main function;
 const validateForm = (event) => {
 	 event.preventDefault();
-let form = document.querySelector('#myForm');
+
 let first = document.querySelector('#firstName').value;
 let last = document.querySelector('#lastName').value;
 let email = document.querySelector('#email').value;
 let company = document.querySelector('#company').value;
 let phone = document.querySelector('#tel').value;
 let errorMsg = document.querySelector('#error-message');
-
 let sayError;
-//new function declared;
-let isValidate;
-let validate = () => {
-	isValidate = false;
-	if (!isValidate) {
-		validateForm();
-	}
+
 if(first === "" || last === "" || company == "") {
-	 errorMsg = 'sorry input field can not be empty, please enter fields';
+	 sayError = 'sorry input field can not be empty, please enter fields';
 	 errorMsg.innerText = sayError;
-	 return  false;
+	 return 
  } if(email.indexOf('@') == -1 || email === '') {
-	errorMsg = 'email not valid please enter correct address';
+	sayError = 'email not valid please enter correct address';
 	errorMsg.innerText = sayError;
-   return false;
+	return 
 	 } 
 	 alert('form submitted successfully!')
-	 return true;
-}
+	 return 
 }
 document.querySelector('#formId').addEventListener('submit', validateForm);
 
