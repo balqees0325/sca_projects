f9c97a0589f1abb172dbebbbf50330ca0e420cae
balib01 <balib1437@gmail.com>
2021-04-02 23:05:04 +0100

    completed

--- SCA/formTask/profile.js
+++ SCA/formTask/profile.js
@@ -1,14 +1,17 @@
 let button = document.querySelector('button');
 let displayProfile = document.querySelector('.userdata')
+
 const displayData = () =>{
+  console.log('hi');
   let {username, phone, email,password} = JSON.parse(window.localStorage.getItem("formInput"));
+
   displayProfile.innerHTML = `
     h2>Hello ${username}</h1>
     <div>
     <h2>Registration Details Below</h2>
     <p>Phone Number: ${phone}</p>
     <p>Email Address: ${email}</p>
-    <p>Password: ${Password}</p>
+    <p>Password: ${password}</p>
     </div>
   `;
 
@@ -19,7 +22,8 @@ const displayData = () =>{
 		console.log(`${loginInput}`);
 		window.localStorage.clear("loginInput");
 		alert(`You have succefully logout`);
+    window.location.href = '/index.html';
  };
 
-document.body.addEventListener("load", displayData);
+displayData();
 button.addEventListener("click", clearUserData);
